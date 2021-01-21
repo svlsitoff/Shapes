d0c9bbe4785fceebf279a82874ad8fd4bcb077dd
svlsitoff <svlsitoff@gmail.com>
2021-01-22 00:08:26 +0800

    initial

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestShapes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestShapes")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cefafa65-fd78-49fc-a5a5-46a8651806a5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestShape.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibShapes;
+
+namespace UnitTestShapes
+{
+    [TestClass]
+    public class UnitTestShape
+    {
+        [TestMethod]
+        public void TestCircle()
+        {
+            Shape circle = new Shape(10);
+            double check = 314;
+            Assert.AreEqual(check, circle.getArea());
+          
+        }
+        [TestMethod]
+        public void TestErrorCircle()
+        {         
+            Shape errorcircle = new Shape();
+            double check = 0;
+            Assert.AreEqual(0, errorcircle.getArea());
+        }
+        [TestMethod]
+
+        public void TestTriangle()
+        {
+            Shape triangle = new Shape(10,10,12);
+            double check = 48;
+            Assert.AreEqual(check, triangle.getArea());
+        }
+        [TestMethod]
+        public void TestErrorTriangle()
+        {
+            Shape triangle = new Shape(2,3);
+            double check = 0;
+            Assert.AreEqual(check, triangle.getArea());
+        }
+
+
+    }
+}
